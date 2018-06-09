279de3ad91329793b56f2711a56de6a8d23c66da
Naoaki Hashimoto <hasimoto0130gt@gmail.com>
2018-06-09 16:12:28 +0900

    initial app

---
+++ src/main/java/com/example/mtgtimestamp/MtgTimestampApplication.java
@@ -0,0 +1,12 @@
+package com.example.mtgtimestamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgTimestampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtgTimestampApplication.class, args);
+	}
+}
